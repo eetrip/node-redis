a8d28ba4435bde0d641d12918539755b83b598e8
eetrip <eeshantripathi111@gmail.com>
2020-02-29 12:53:51 +0530

    prettier applied

--- server.js
+++ server.js
@@ -1,43 +1,43 @@
-let express = require('express')
-let path = require('path')
-let bodyParser = require('body-parser')
-let methodOverride = require('method-override')
-let redis = require('redis')
-let client = redis.createClient()
+let express = require("express");
+let path = require("path");
+let bodyParser = require("body-parser");
+let methodOverride = require("method-override");
+let redis = require("redis");
+let client = redis.createClient();
 
 // define routes
-let routes = require('./Routes/api')
+let routes = require("./Routes/api");
 
 // Set Port
-let PORT = 4040
+let PORT = 4040;
 
 // Init app
-let app = express()
+let app = express();
 
-// instantiate a connection to redis 
-client.on('connect', () => {
-  console.log('Connected to Redis...')
-})
+// instantiate a connection to redis
+client.on("connect", () => {
+  console.log("Connected to Redis...");
+});
 
 // body-parser
-app.use(bodyParser.urlencoded({extended:true}))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // methodOverride
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 
 // Routes Middleware
-app.use(routes)
+app.use(routes);
 
 // 404 handler
 app.use((req, res) => {
-  res.status(404)
-  res.send('endpoint not found')
-})
+  res.status(404);
+  res.send("endpoint not found");
+});
 
 // serve application on specified port
 app.listen(PORT, () => {
-  console.log('Server started on port ' + PORT)
+  console.log("Server started on port " + PORT);
 });
 
-module.exports = app
+module.exports = app;
